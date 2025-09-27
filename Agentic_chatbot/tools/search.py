73b7d00414ce8b09af6f53ad0cb8110edd695fd4
Jayyyu1w <jayyu1w@gmail.com>
2025-09-27 18:07:23 +0800

    feat: add project structure

---
+++ Agentic_chatbot/tools/search.py
@@ -0,0 +1,19 @@
+import os
+import getpass
+from typing import Literal
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
+
+class SearchTool:
+    def __init__(self, search_engine=Literal["tavily", "duckduckgo"], search_results=5):
+        if search_engine == "tavily":
+            if not os.environ.get("TAVILY_API_KEY"):
+                os.environ["TAVILY_API_KEY"] = getpass.getpass("Enter your Tavily API key: ")
+            self.search_engine = TavilySearchResults(max_results=search_results)
+        elif search_engine == "duckduckgo":
+            wrapper = DuckDuckGoSearchAPIWrapper(region="tw-tzh", max_results=search_results)
+            self.search_engine = DuckDuckGoSearchRun(api_wrapper=wrapper)
+
+    def search(self, query):
+        return self.search_engine.invoke(query)
